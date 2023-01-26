7289f43b9f73d10a894c028c5c355616bafff94e
ZakicAleksa <93937431+ZakicAleksa@users.noreply.github.com>
2023-01-26 02:07:26 +0100

    initial front impl

---
+++ Hospital-Front/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MaterialModule } from "./material/material.module";
+import { HospitalModule } from "./modules/hospital/hospital.module";
+import { PagesModule } from "./modules/pages/pages.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatStepperModule} from "@angular/material/stepper";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule,
+    PagesModule,
+    HospitalModule,
+    MatTabsModule,
+    MatStepperModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
